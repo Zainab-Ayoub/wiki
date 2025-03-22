dc0822e4c6502bcb6359b5e9daaad986da379a1a
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-03-22 16:25:49 +0500

    formatting done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -11,11 +10,21 @@ def index(request):
 # step 1 : entry page function
 
 def entry_page(request, title):
-    content = util.get_entry(title)
-    print(f"Title: {title}, Content: {content}") 
-    if content == None:
+    entries = util.list_entries()
+    for entry in entries:
+        # print entry on  console
+        print(f"Checking entry: {entry}")
+        if entry.lower() == title.lower():
+            title = entry
+            content = util.get_entry(title)
+            content = markdown2.markdown(content)
+            # print title and content on console
+            print(f"Title: {title}, Content: {content}") 
+            return render(request, 'encyclopedia/entry.html', {
+                'title': title,
+                'content': content,    
+            })
+            
+    else:
         return render(request, 'encyclopedia/error.html', {'message':'Page not Found!'})
-    return render(request, 'encyclopedia/entry.html', {
-        'title': title,
-        'content': content,    
-    })
+    
