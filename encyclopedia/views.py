ca4ea18f9d2c9cd1e6cac4c661e4f18aa0082f21
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-03-22 15:59:55 +0500

    entry_page function made to get entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# step 1 : entry page function
+
+def entry_page(request, title):
+    content = util.get_entry(title)
+    print(f"Title: {title}, Content: {content}") 
+    if content == None:
+        return render(request, 'encyclopedia/error.html', {'message':'Page not Found!'})
+    return render(request, 'encyclopedia/entry.html', {
+        'title': title,
+        'content': content,    
+    })
